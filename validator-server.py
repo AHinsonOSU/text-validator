2ca24994278c2f2d418be03596115805b1f39ae2
AHinsonOSU <hinsonal@oregonstate.edu>
2026-06-05 11:27:41 -0700

    Refactor uvicorn startup entry point
    Updated the entry point for uvicorn to use 'validator-server:app'.

--- validator-server.py
+++ validator-server.py
@@ -63,7 +63,7 @@ def validate(req: ValidationRequest):
     }
 
 #
-# Handle the uvicorn calls in the code itself.
+# Init function for the uvicorn startup
 #
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("validator-server:app", host="127.0.0.1", port=8000, reload=True)
